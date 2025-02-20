4beca213c6e825258c89d2d88a3e78fd40a1b970
daizengtao <51010873+902D9@users.noreply.github.com>
2025-02-20 18:07:25 +0800

    Add files via upload

---
+++ Source/GeometryScriptingPlus/Private/GeometryScriptingPlusBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GeometryScriptingPlusBPLibrary.h"
+#include "GeometryScriptingPlus.h"
+
+UGeometryScriptingPlusBPLibrary::UGeometryScriptingPlusBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UGeometryScriptingPlusBPLibrary::GeometryScriptingPlusSampleFunction(float Param)
+{
+	return -1;
+}
+
